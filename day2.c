e7a241cf008d8aaef7e685ec8d75e3207112e8b8
sunainakumari8990-gif <sunainakumari8990@gmail.com>
2025-12-17 17:04:55 +0530

    second

---
+++ day2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int multiply()
+{
+int a=10;
+int b=20;
+printf("multiply= %d",a*b);
+
+}
+int divide()
+{
+int a=40;
+int b=20;
+printf("\ndivide= %d",a/b);
+
+}
+
+int main()
+{
+multiply();
+divide();
+
+
+}
